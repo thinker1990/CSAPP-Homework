b8429e1e1075211fd2787aac6179884591c48c44
thinker1990 <liumkscut@gmail.com>
2017-09-26 22:16:02 +0800

    add homework 2.89 ~ 2.91

---
+++ homework/chapter02/2_89.c
@@ -0,0 +1,5 @@
+A. (float) x == (float) dx   :   True
+B. dx - dy == (double) (x - y)   :   False   eg. x - y overflow
+C. (dx + dy) + dz == dx + (dy + dz)  :  True
+D. (dx * dy) * dz == dx * (dy * dz)  :  False   eg. dx * dy = infinity
+E. dx / dx == dz / dz  :  False  eg. dx = 0 cause dx / dx = NaN
---
+++ homework/chapter02/2_91.c
@@ -0,0 +1,3 @@
+A. 0x490FDB
+B. 11.001001(001)
+C. 9th hint: use show_bytes
